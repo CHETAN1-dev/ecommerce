b26af219b8d1e1e64c51034d4af203665ddeb341
Chetan <chetan.cm45@gmail.com>
2025-03-30 13:42:19 +0530

    d

---
+++ src/product/dto/create-product.dto.ts
@@ -0,0 +1,17 @@
+import { IsString, IsNumber, Min, IsOptional } from '@nestjs/class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  name: string;
+
+  @IsString()
+  description: string;
+
+  @IsNumber()
+  @Min(0)
+  price: number;
+
+  @IsOptional()
+  @IsString()
+  category?: string;
+}
---
+++ src/product/product.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Product } from './product.entity';
+
+@Injectable()
+export class ProductService {
+  constructor(
+    @InjectRepository(Product)
+    private productRepository: Repository<Product>,
+  ) {}
+
+  findAll(): Promise<Product[]> {
+    return this.productRepository.find();
+  }
+
+  create(product: Partial<Product>): Promise<Product> {
+    const newProduct = this.productRepository.create(product);
+    return this.productRepository.save(newProduct);
+  }
+}
